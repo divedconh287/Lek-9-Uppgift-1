22976df3bd5fc4b2fd22d27b076a10caa85f8a7f
Aaron Granath TE24C <divedconh287@gmail.com>
2025-10-17 10:27:37 +0200

    på tåget

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-let ul = document.querySelector("ul");
-let li = document.createElement("li");
-ul.appendChild(li);
+function laggtill(){
+console.log("funka");
+let text = document.querySelector("#skriv");
+let text = document.createElement("li");
+
+text.textcontext=laggtill.value;
+document.ul.appendChild(text);
+}
+
+// let ul = document.querySelector("ul");
+// let li = document.createElement("li");
+// ul.appendChild(li);
